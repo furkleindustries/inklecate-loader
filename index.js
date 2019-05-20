fc9e88d3fe6d43500045e6408b2fadc41ec058bd
furkle <furkle@furkleindustries.com>
2019-05-20 11:27:44 -0400

    First commit.

---
+++ index.js
@@ -0,0 +1,21 @@
+const {
+  getCacheFilePath,
+  inklecate,
+} = require('inklecate');
+
+module.exports = function InkWebpackLoader(source) {
+  const callback = this.async();
+  this.addDependency(getCacheFilePath());
+  inklecate([ this.resourcePath ]).then(
+    function resolved(data) {
+      callback(
+        `export const storyContent = ${JSON.stringify(data.storyContent)};\n` +
+          `export const text = ${JSON.stringify(source.trim())};\n` +
+          `export const compilerOutput = ` +
+            `${JSON.stringify(data.compilerOutput)};\n`
+      );
+    },
+
+    function rejected(err) { throw err; },
+  );
+};
